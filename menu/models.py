2c485efba48e0fbc7c3e2c88a6984e4959060987
Lucca Trevisan <141439341+luccatrevisan@users.noreply.github.com>
2026-01-23 17:19:56 -0300

    add ImageField to models.py, MEDIA_ROOT and static() to urlpatterns

--- menu/models.py
+++ menu/models.py
@@ -16,6 +16,7 @@ class MenuItem(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.TextField(max_length=300, blank=False, null=False)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    image = models.ImageField(upload_to='menu_photos', blank=False, null=False)
 
     def __str__(self):
         return self.name
