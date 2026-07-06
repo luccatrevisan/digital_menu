bef94bf1a2dd768416b2fba625695b58b21a9e4b
lucca <luccatrevisandev@gmail.com>
2026-07-06 12:07:21 -0300

    fix static files configs

--- setup/settings.py
+++ setup/settings.py
@@ -117,10 +117,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Media Files
 
